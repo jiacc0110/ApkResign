714200c384b351dbaad206e8b71f181c70dc886b
jiacc <jiacc0110@163.com>
2022-07-14 14:16:38 +0800

    Update resign.bat

--- resign.bat
+++ resign.bat
@@ -1,5 +1,5 @@
 call properties.bat
 
-%Android_Tools%\29.0.2\zipalign -v 4 %inputApk% out_apk_aligned_unsigned.apk 
+%Android_Tools%\%Build_Tools_Version%\zipalign -v 4 %inputApk% out_apk_aligned_unsigned.apk 
 java -jar %Android_Tools%\bin\apksigner sign --ks key_file --ks-key-alias key0 out_apk_aligned_unsigned.apk
 rename out_apk_aligned_unsigned.apk out_apk_aligned_signed.apk
